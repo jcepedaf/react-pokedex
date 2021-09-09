515ad406ebcd8d6d8fa76b580193f4ad52715eaa
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-09 22:18:07 +0200

    agrego inClass exercise E

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React from 'react';
 export function App() {
   return (
     <div>
+      <Logo/>
+      </div>
+  )
+}
+
+export function Logo() {
+
+  return (
       <header><h1>Welcome to the Pokedex</h1>
       <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
       </header>
-      </div>
   )
 }
 
