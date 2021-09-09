58760d711f00352cc557db55985d4014b57bcbf0
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-09 21:50:53 +0200

    agrego inClass exercise C

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-//exercise B
+// //exercise B
 
-const element = <h1>Hello Code Your Future</h1>
+// const element = <h1>Hello Code Your Future</h1>
 
-    const rootElement = document.getElementById('root')
-    ReactDOM.render(element, rootElement)
+//     const rootElement = document.getElementById('root')
+//     ReactDOM.render(element, rootElement)
 
-//end exercise B
+// //end exercise B
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+//exercise C
 
+  ReactDOM.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+   document.getElementById('root')
+ );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
+
+//end exercise C
 
 
