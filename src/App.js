585b52fa2107c2d57ec87506234dcb63a08d6b5b
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-09 23:30:22 +0200

    agrego inClass exercise G

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ export function App() {
   return (
     <div>
       <Logo/>
+      <CaughtPokemon/>
+      <BestPokemon/>
       </div>
   )
 }
@@ -17,9 +19,35 @@ export function App() {
 //   )
 // }
 
- const Logo = () => (
-      <header><h1>Welcome to the Pokedex</h1>
+ const Logo = () => {
+   const appName = "Pokedex";
+   return (
+      <header><h1>{appName}</h1>
       <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
       </header>);
+ }
+
+ const CaughtPokemon = () => {
+   const date = new Date().toLocaleDateString();
+
+   return (
+     <p>Caught 0 Pokémon on {date}</p>
+
+   )
+ } 
+
+ const BestPokemon = () => {
+   const abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
+   return (
+     <div>
+       <p>
+         <ul>
+           {abilities.map(ability => <li>{ability}</li>)}
+         </ul>
+       </p>
+     </div>
+
+   )
+ }
 
 export default App;
