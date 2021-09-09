066e8232d86423933785107cc7fb8dc94835ecee
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-09 22:13:07 +0200

    agrego inClass exercise D

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header><h1>Welcome to the Pokedex</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
       </header>
-    </div>
-  );
+      </div>
+  )
 }
+
+
 
 export default App;
