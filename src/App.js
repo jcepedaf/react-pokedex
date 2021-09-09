1f8ba641f7d2bb0773b72c7410ae0bf0b635992a
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-09 23:03:43 +0200

    agrego inClass exercise F

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ export function App() {
   )
 }
 
-export function Logo() {
+// export function Logo() {
 
-  return (
+//   return (
+//       <header><h1>Welcome to the Pokedex</h1>
+//       <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
+//       </header>
+//   )
+// }
+
+ const Logo = () => (
       <header><h1>Welcome to the Pokedex</h1>
       <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
-      </header>
-  )
-}
-
-
+      </header>);
 
 export default App;
