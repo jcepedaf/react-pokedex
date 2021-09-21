66b6614bf6c8257646551e010596428c6f9ba819
jcepedaf <jcepedaf@gmail.com>
2021-09-21 20:51:18 +0200

    agrego inClass G de react semana 3

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -2,15 +2,25 @@ import React, {useState} from 'react'
 
 const CaughtPokemon = (props) => {
   const [caught, setCaught] = useState([]);
+  const [error, setError] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
+
   const catchPokemon = () => { 
-    const pokemons = ["Pikachu", "Snorlax", "Bulbasur", "Charizard", "Mew", "Mewtwo", "Pidgey"];
-    const random = Math.floor(Math.random() * pokemons.length);
-    setCaught(caught.concat(pokemons[random])) 
+    if(pokemonNameInput === "") {
+      setError(true);
+    } else {
+      setError(false);
+      setCaught(caught.concat(pokemonNameInput));
+      setPokemonNameInput("");
+    }
+     
   };
  
     return (
       <>
+      <input type="text" value={pokemonNameInput} onChange={(e)=>setPokemonNameInput(e.target.value)}/>
       <button onClick={catchPokemon}>Catch!!!</button>
+      {error ? <p>hubo un error</p> : null}
       <p>Caught {caught.length} Pokémon on {props.date}</p>
       <ul>
         {caught.map((pokemon, index) => <li key={index}>{pokemon}</li>)}
